f3f38acc401eaf0d54431eec2110cd7ab685e5dd
ReV3rb698 <146119522+ReV3rb698@users.noreply.github.com>
2024-05-29 09:12:49 -0700

    Add files via upload

--- controller/database_controller.js
+++ controller/database_controller.js
@@ -18,100 +18,100 @@ const databaseController = {
             res.redirect('/petIndex');
         }
     },
-    editPet: async (req, res) => {
-        let Name = req.body.name;
-        let Gender = req.body.gender;
-        let Specie = req.body.specie;
-        let Breed = req.body.breed;
-        let BirthDate = req.body.birthdate;
-        let Description = req.body.description;
-        let MedName = req.body.medName;
-        let MedDescription = req.body.medDescription;
-        let BodyPart = req.body.bodyPart;
-        let Symptom = req.body.symptom;
-        let ConDescription = req.body.conDescription;
-        let Weight = req.body.weight;
-        let WeightDate = req.body.weightDate;
-        let UserName = req.body.userName;
-        let petId = req.params.id;
-        
-        try {
-            const [rows] = await promiseUserPool.query("SELECT P.*, M.MedName, M.Description as MedDescription, C.BodyPart, C.Symptom, C.Description as ConDescription, W.Weight, W.Date, U.name as UserName FROM PET P LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID LEFT JOIN users U ON OI.UserID = U.ID WHERE P.PetID = ?", [petId]);
-            const currentInfo = rows[0];
+    editPet:  async (req, res) => {
+      let Name = req.body.name;
+      let Gender = req.body.gender;
+      let Specie = req.body.specie;
+      let Breed = req.body.breed;
+      let BirthDate = req.body.birthdate;
+      let Description = req.body.description;
+      let MedName = req.body.medName;
+      let MedDescription = req.body.medDescription;
+      let BodyPart = req.body.bodyPart;
+      let Symptom = req.body.symptom;
+      let ConDescription = req.body.conDescription;
+      let Weight = req.body.weight; 
+      let WeightDate = req.body.weightDate;
+      let UserName = req.body.userName;
+      let petId = req.params.id;
+      
+      try {
+          const [rows] = await promiseUserPool.query("SELECT P.*, M.MedName, M.Description as MedDescription, C.BodyPart, C.Symptom, C.Description as ConDescription, W.Weight, W.Date, U.name as UserName FROM PET P LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID LEFT JOIN users U ON OI.UserID = U.ID WHERE P.PetID = ?", [petId]);
+          const currentInfo = rows[0];
 
-            if (Name === "") {
-                Name = currentInfo.Name;
-            }
-            if (Gender === "") {
-                Gender = currentInfo.Gender;
-            }
-            if (BirthDate === "") {
-                BirthDate = currentInfo.BirthDate;
-            }
-            if (Specie === "") {
-                Specie = currentInfo.Specie;
-            }
-            if (Breed === "") {
-                Breed = currentInfo.Breed;
-            }
-            if (Description === "") {
-                Description = currentInfo.Description;
-            }
-            if (MedName === "") {
-                MedName = currentInfo.MedName;
-            }
-            if (MedDescription === "") {
-                MedDescription = currentInfo.MedDescription;
-            }
-            if (BodyPart === "") {
-                BodyPart = currentInfo.BodyPart;
-            }
-            if (Symptom === "") {
-                Symptom = currentInfo.Symptom;
-            }
-            if (ConDescription === "") {
-                ConDescription = currentInfo.ConDescription;
-            }
-            if (Weight === "") {
-                Weight = currentInfo.Weight;
-            }
-            if (UserName === "") {
-                UserName = currentInfo.UserName;
-            }
+          if (Name === "") {
+              Name = currentInfo.Name;
+          }
+          if (Gender === "") {
+              Gender = currentInfo.Gender;
+          }
+          if (BirthDate === "") {
+              BirthDate = currentInfo.BirthDate;
+          }
+          if (Specie === "") {
+              Specie = currentInfo.Specie;
+          }
+          if (Breed === "") {
+              Breed = currentInfo.Breed;
+          }
+          if (Description === "") {
+              Description = currentInfo.Description;
+          }
+          if (MedName === "") {
+              MedName = currentInfo.MedName;
+          }
+          if (MedDescription === "") {
+              MedDescription = currentInfo.MedDescription;
+          }
+          if (BodyPart === "") {
+              BodyPart = currentInfo.BodyPart;
+          }
+          if (Symptom === "") {
+              Symptom = currentInfo.Symptom;
+          }
+          if (ConDescription === "") {
+              ConDescription = currentInfo.ConDescription;
+          }
+          if (Weight === "") {
+              Weight = currentInfo.Weight;
+          }
+          if (UserName === "") {
+              UserName = currentInfo.UserName;
+          }
 
+          
+          console.log(Name, Gender, BirthDate, Breed, Description, UserName, petId, Weight, WeightDate);
+
+          await promiseUserPool.query(`
+          UPDATE PET P
+          LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID
+          LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID
+          LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID
+          LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID
+          LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID
+          LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID
+          LEFT JOIN users ON OI.UserID = users.id
+          LEFT JOIN users U ON OI.UserID = U.ID
+          SET P.Name = ?,
+              P.Gender = ?,
+              P.BirthDate = ?,
+              P.Breed = ?,
+              P.Description = ?,
+              U.name = ?
+          WHERE P.PetID = ?
+      `, [Name, Gender, BirthDate, Breed, Description, UserName, petId]);
+
+          if (Weight !== "" && WeightDate !== "") {
+              await promiseUserPool.query('INSERT INTO WEIGHTCHECK (PetID, Weight, Date) VALUES ((SELECT DISTINCT w.PetID FROM WEIGHTCHECK w JOIN PET p ON w.PetID = p.PetID WHERE p.Name = ?), ?, ?)',[Name, Weight, WeightDate]);
+          }
+          
+          res.redirect('/petProfile/' + petId);
+      } catch (error) {
+          console.error(error);
+      }
+  },
             
-            console.log(Name, Gender, BirthDate, Breed, Description, UserName, petId);
-
-            await promiseUserPool.query(`
-            UPDATE PET P
-            LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID
-            LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID
-            LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID
-            LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID
-            LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID
-            LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID
-            LEFT JOIN users ON OI.UserID = users.id
-            LEFT JOIN users U ON OI.UserID = U.ID
-            SET P.Name = ?,
-                P.Gender = ?,
-                P.BirthDate = ?,
-                P.Breed = ?,
-                P.Description = ?,
-                U.name = ?
-            WHERE P.PetID = ?
-        `, [Name, Gender, BirthDate, Breed, Description, UserName, petId]);
             
-            // let Weight = req.body.weight; 
-
-            // if (Weight !== "" && WeightDate !== "") {
-            //     await promiseUserPool.query('INSERT INTO WEIGHTCHECK (PetID, Weight, Date) VALUES ((SELECT DISTINCT w.PetID FROM WEIGHTCHECK w JOIN PET p ON w.PetID = p.PetID WHERE p.Name = ?), ?, ?)',[Name, Weight, WeightDate]);
-            // }
-            
-            res.redirect('/petProfile/' + petId);
-        } catch (error) {
-            console.error(error);
-        }
-    },
     deletePet: async (req, res) => {
         try {
             await promiseUserPool.query('DELETE FROM PET WHERE id = ?', req.params.id);
@@ -122,44 +122,53 @@ const databaseController = {
         }
     },
     getPetbyID: async (req, res, next) => {
+        console.log("getPetbyID called")
         try {
-            const petId = req.params.id;
+          const petId = req.params.id;
+      
+          // Fetch basic pet information
+          const [petInfoRows] = await promiseUserPool.query('SELECT * FROM Pet_View WHERE PetID = ?', [petId]);
+          req.petInfo = petInfoRows.length > 0 ? petInfoRows[0] : null;
+      
+          // Fetch pet image
+          const [petImageRows] = await promiseUserPool.query('SELECT * FROM Pet_Image_Tag_List WHERE PetID = ?', [petId]);
+          req.petImage = petImageRows.length > 0 ? petImageRows[0] : null;
+      
+          // Fetch detailed pet information
+          const [rows] = await promiseUserPool.query(`
+            SELECT P.*, PMI.Portion, PMI.Rate, PMI.Date as pmiDate, M.MedName, M.Description as MedDescription, C.BodyPart, C.Symptom, C.Description as ConDescription, W.Weight, W.Date, U.name as UserName
+            FROM PET P
+            LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID
+            LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID
+            LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID
+            LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID
+            LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID
+            LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID
+            LEFT JOIN users U ON OI.UserID = U.ID
+            WHERE P.PetID = ?
+          `, [petId]);
+      
+          if (rows.length > 0) {
+            req.pet = rows[0];
+            next();
+          } else {
+            res.status(404).send('Pet not found');
+          }
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Error getting pet');
+        }
+      },
+    getPetsbyUserID: async (req, res, next) => {
+        console.log("getPetsbyUserID called")
+        try {
+            const userId = req.user.id;  // Assuming the user's ID is stored in req.user.id
 
-            const [petInfo] = await promiseUserPool.query('SELECT * FROM Pet_View WHERE PetID = ?', petId);
+            const [petInfo] = await promiseUserPool.query('SELECT * FROM Pet_View WHERE UserID = ?', userId);
 
             if (petInfo.length > 0) {
                 req.petInfo = petInfo[0];
             }
-
-            const [rows] = await promiseUserPool.query(`
-                SELECT P.*, PMI.Portion, PMI.Rate, PMI.Date as pmiDate, M.MedName, M.Description as MedDescription, C.BodyPart, C.Symptom, C.Description as ConDescription, W.Weight, W.Date, U.name as UserName
-                FROM PET P
-                LEFT JOIN PET_MED_INT PMI ON P.PetID = PMI.PetID
-                LEFT JOIN MEDICATION M ON PMI.MedID = M.MedID
-                LEFT JOIN PET_CON_INT PCI ON P.PetID = PCI.PetID
-                LEFT JOIN CONDITIONS C ON PCI.ConditionID = C.ConditionID
-                LEFT JOIN WEIGHTCHECK W ON P.PetID = W.PetID
-                LEFT JOIN OWNERSHIP_INT OI ON P.PetID = OI.PetID
-                LEFT JOIN users U ON OI.UserID = U.ID
-                WHERE P.PetID = ?
-                `, petId);
-    
-            if (rows.length > 0) {
-                req.pet = rows[0];
-                
-
-                next();
-            } else {
-                res.status(404).send('Pet not found');
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error getting pet');
-        }
-    },
-    getPetsbyUserID: async (req, res, next) => {
-        try {
-            const userId = req.user.id;  // Assuming the user's ID is stored in req.user.id
 
             const [rows] = await promiseUserPool.query(`
                 SELECT *
@@ -189,55 +198,58 @@ const databaseController = {
         }
     },
     getLatestWeightCheck: async (req, res, next) => {
+        console.log("getLatestWeightCheck called")
         try {
-        const [results] = await promiseUserPool.query('SELECT * FROM WEIGHTCHECK wc WHERE wc.PetID = ? ORDER BY wc.Date DESC LIMIT 1', [req.params.id]);
+        const [results] = await promiseUserPool.query('SELECT * FROM WEIGHTCHECK wc WHERE wc.PetID = ? ORDER BY wc.WCID DESC LIMIT 1', [req.params.id]);
         if (results.length > 0) {
             req.weight = results[0];
             next();
         } else {
-            res.status(404).send('Weight check not found');
+            req.weight = null;
+            next();
         } } catch (err) {
             console.error(err);
             res.status(500).send('Error getting weight check');
         }
 },
     getPreivousWeightCheck: async (req, res, next) => {
+        console.log("getPreviousWeightCheck called")
         try {
-            const [results] = await promiseUserPool.query('SELECT * FROM WEIGHTCHECK wc WHERE wc.PetID = ? ORDER BY wc.Date DESC LIMIT 1, 1', [req.params.id]);
+            const [results] = await promiseUserPool.query(`SELECT * FROM (
+                SELECT * FROM WEIGHTCHECK wc WHERE wc.PetID = ? ORDER BY wc.WCID DESC LIMIT 2
+            ) AS subquery ORDER BY WCID ASC LIMIT 1`, [req.params.id]);
             if (results.length > 0) {
                 req.prevWeight = results[0];
                 next();
             } else {
-                res.status(404).send('Weight check not found');
+                req.prevWeight = null;
+                next();
             } } catch (err) {
                 console.error(err);
                 res.status(500).send('Error getting weight check');
             }
     },
     getRemindersforPet: async (req, res, next) => {
+        console.log("getRemindersforPet called");
         try {
-            const petId = req.params.id;
-
-            const [messagesRows] = await promiseUserPool.query('SELECT * FROM Urgent_list WHERE PetID = ?', petId);
-
-            if (messagesRows.length > 0) {
-                req.messages = messagesRows;
-            }
-            
-            const [rows] = await promiseUserPool.query('SELECT * FROM Reminder_List WHERE PetID = ?', petId);
-
-            if (rows.length > 0) {
-                req.reminders = rows;
-                next();
-            } else {
-                res.status(404).send('No reminders found for this pet');
-            }
+          const petId = req.params.id;
+      
+          // Fetch messages
+          const [messagesRows] = await promiseUserPool.query('SELECT * FROM Urgent_list WHERE PetID = ?', [petId]);
+          req.messages = messagesRows.length > 0 ? messagesRows : [];
+      
+          // Fetch reminders
+          const [rows] = await promiseUserPool.query('SELECT * FROM Reminder_List WHERE PetID = ?', [petId]);
+          req.reminders = rows.length > 0 ? rows : [];
+      
+          next();
         } catch (error) {
-            console.error(error);
-            res.status(500).send('Error getting reminders');
+          console.error(error);
+          res.status(500).send('Error getting reminders');
         }
-    },
+      },
     getPrescriptionsForPet: async (req, res, next) => {
+        console.log("getPrescriptionsForPet called")
         try {
             const petId = req.params.id;
 
@@ -247,13 +259,46 @@ const databaseController = {
                 req.prescriptions = rows;
                 next();
             } else {
-                res.status(404).send('No prescriptions found for this pet');
+                req.prescriptions = [];
+                next();
             }
         } catch (error) {
             console.error(error);
             res.status(500).send('Error getting prescriptions');
         }
-    }
+    },
+    createPrescription: async (req, res) => {
+        console.log("createPrescription called");
+        const prescriptionInfo = {
+            MedName: req.body.medName,
+            Description: req.body.description
+        };
+        console.log(req.body);
+        try {
+            const [currentMeds] = await promiseUserPool.query('SELECT * FROM MEDICATION WHERE MedName = ?', req.body.medName);
+            if (currentMeds.length === 0) { // If the medication does not exist in the database, insert it
+                await promiseUserPool.query("INSERT INTO MEDICATION SET `MedName` = ?, `Description` = ?", [req.body.medName, req.body.description]);
+            }
+    
+            const [pet] = await promiseUserPool.query('SELECT * FROM PET WHERE PetID = ?', req.body.petName); // Assuming PetID is used in the dropdown
+            console.log(pet);
+            if (pet.length === 0) {
+                throw new Error(`Pet not found for ID: ${req.body.petName}`);
+            }
+            
+            await promiseUserPool.query("INSERT INTO PET_MED_INT (PetID, MedID, Portion, Rate, Date) VALUES (?, (SELECT MedID FROM MEDICATION WHERE MedName = ?), ?, ?, ?)", [pet[0].PetID, req.body.medName, req.body.portion, req.body.rate, req.body.date]);
+    
+            res.redirect('/petProfile/' + req.body.petName);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(`Error creating prescription: ${error.message}`);
+        }
+    },
+    
+    
+    
+    
+    
 };
 
 
